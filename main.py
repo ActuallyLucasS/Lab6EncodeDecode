0ad05591b496a68041d4c16f980720caada9ea86
Aaron Gallego <aarongallego090@gmail.com>
2023-10-24 12:56:37 -0400

    updated

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+# Lucas Stultz
+
+
+
 if __name__ == "__main__":
     while True:
-        print("Menu\n" + "-------------\n" + "1. Encode\n" + "2. Decode\n" + "3. Quit")
+        print("\nMenu\n" + "-------------\n" + "1. Encode\n" + "2. Decode\n" + "3. Quit")
         option = int(input("\nPlease enter an option: "))
         if option == 1:
             password = input("Please enter your password to encode: ")
             encoded_pass = ""
             for i in password:
-                encoded_digit = str((int(i) + 3) % 10) # mod by 10 so it doesnt go over 10 (EX: 7 + 3 = 10, remainder will be 0)
-                encoded_pass += encoded_digit # add to encode
+                encoded_digit = str((int(i) + 3) % 10)  # mod by 10 so it doesn't go over 10 (EX: 7 + 3 = 10, remainder will be 0)
+                encoded_pass += encoded_digit   # add to encode
             print("Your password has been encoded and stored!\n")
-        elif option == 2:
-            pass
+        elif option == 2:  # Aaron Gallego did Decode
+            decoded_pass = ""
+            for i in encoded_pass:
+                decoded_digit = str((int(i) - 3) % 10)
+
+                decoded_pass += decoded_digit
+
+            print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
         elif option == 3:
             break
         else:
